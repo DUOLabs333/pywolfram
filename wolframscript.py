7bf08e585511770a3b712524e9e9398b6ee596ab
DUOLabs <dvdugo333@gmail.com>
2021-06-05 11:19:38 -0400

    Update wolframscript.py

--- wolframscript.py
+++ wolframscript.py
@@ -2,18 +2,28 @@
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wlexpr
 import readline
+Quit=['Quit','Quit[]', 'Exit', 'Exit[]']
 
 count=0
 
 session=WolframLanguageSession('/cygdrive/c/Users/School/Downloads/Mathematica/WolframKernel.exe')
 session.start()
-session.evaluate(wlexpr('D[x^2,x]'))
+session.evaluate(wlexpr('Unprotect[Out];'))
 print('\nWolfram Language 12.2.0 Engine for Microsoft Windows (64-bit)\n Copyright 1988-2021 Wolfram Research, Inc.\n')
 try:
     while True:
-        expr='ToString['+input('In['+str(count)+']:= ')+',InputForm]'
-        print('Out['+str(count)+']= '+str(session.evaluate(wlexpr(expr))))
-        print()
+        expr=input('In['+str(count)+']:= ')
+        if expr in Quit:
+            raise KeyboardInterrupt
+        
+        session.evaluate(wlexpr(f'$Line={count};'))
+        
+        result=session.evaluate(wlexpr('ToString['+expr+',InputForm]'))
+        
+        session.evaluate(wlexpr(f'Out[{count}]={result};'))
+        
+        print(f'Out[{count}]={result}\n')
+        
         count+=1
 except KeyboardInterrupt:
     print()
